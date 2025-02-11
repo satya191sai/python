235557a3980b58f8cbb18b67dfa1725b93cc8293
satya191sai <sai67038@gmail.com>
2025-02-11 23:49:56 +0530

    python programs

---
+++ key.py
@@ -0,0 +1,43 @@
+# import keyword
+
+# printing all keywords at once using "kwlist()"
+# print("The list of keywords is : ");
+# And=10;
+# Self=100;
+# print(Self);
+# print(And);
+# print(type(And));
+# b='sau';
+# print(b);
+# print(i);
+# print(type(b))
+# print(keyword.kwlist);
+'''
+this is prostack;
+
+'''
+#varibles
+Prostack = 100
+print(Prostack )
+
+int =10
+print(int)
+
+
+false = 100
+print(false)
+
+
+
+a = {}
+
+
+
+print(type(a))
+a=10
+b=20
+print(a+b)
+a =int(input())
+b=int(input())
+c =a+b
+print(c)
