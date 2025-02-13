d0bdd10a97aa4d3d8fad7de5296e5b57980e2366
satya191sai <sai67038@gmail.com>
2025-02-13 23:03:24 +0530

    nkj

--- key.py
+++ key.py
@@ -1,17 +1,16 @@
-# import keyword
+import keyword
 
-# printing all keywords at once using "kwlist()"
-# print("The list of keywords is : ");
-# And=10;
-# Self=100;
-# print(Self);
-# print(And);
-# print(type(And));
-# b='sau';
-# print(b);
-# print(i);
-# print(type(b))
-# print(keyword.kwlist);
+print("The list of keywords is : ");
+And=10;
+Self=100;
+print(Self);
+print(And);
+print(type(And));
+b='sau';
+print(b);
+print(i);
+print(type(b))
+print(keyword.kwlist);
 '''
 this is prostack;
 
